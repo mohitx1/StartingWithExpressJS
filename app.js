3689b20e23bd3120fe5277b5c840783d1f3cf527
mohitx1 <mohitrockx3@gmail.com>
2023-09-03 14:35:36 +0530

    changed app.use to app.post

--- app.js
+++ app.js
@@ -1,39 +1,30 @@
-
 const express=require("express");
 const bodyParser=require('body-parser');
-
-
 const app=express();
+const port=3000;
 
 app.use(bodyParser.urlencoded({extended:false}))
 //this will enocde the body that we will receive from input type of form html
 
 
-// app.use("/",(req,res,next)=>{
-//     console.log('This always runs')
-//     next()
-
-// })
-
 
 app.use('/add-product',(req,res,next)=>{
-    // console.log('This is middleware')
     res.send('<form action="/product" method="POST"><input type="text" name="title" ><button type="submit">Add product</button></form>')
-    // next()
 })
 
-app.use("/product",(req,res,next)=>{
+//app.post will be triggered only for post request instead of app.use which used to get triggered for both post as well as get request
+//used app.post instead of app.use
+//app.use("/product",(req,res,next)=>{
+app.post("/product",(req,res,next)=>{
     console.log(req.body)
-    // res.redirect('/add-product')
     res.redirect('/')
-    // res.send("<h1>Welcome to Express</h1>")
-
+//app.post will be triggered only when we visit it through post request otherwise if we visit /product it will just redirectg and not print any thing
 })
 
 app.use("/",(req,res,next)=>{
     res.send("<h1>Welcome to Express</h1>")
-
 })
 
-app.listen(3000)
-
+app.listen(`${port}`,()=>{
+    console.log(`Listening to port: ${port}`)
+})
