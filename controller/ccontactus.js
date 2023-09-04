8e34c536ae00e7a3e5e2fba01d3896b1404df5a0
mohitx1 <mohitrockx3@gmail.com>
2023-09-04 12:29:47 +0530

    minor changes

--- controller/ccontactus.js
+++ controller/ccontactus.js
@@ -1,12 +1,12 @@
 const path=require('path')
 const fs=require('fs')
 
-exports.contactusGet=(req,res,next)=>{
+exports.contactusControllerGet=(req,res,next)=>{
     res.sendFile(path.join(__dirname,"../","views","contactus.html"))
 }
 
 
-exports.contactusPost= (req, res, next) => {
+exports.contactusControllerPost= (req, res, next) => {
     console.log(`Name: ${req.body.name} - Email id: ${req.body.email}`);
     const userDetails = `Name: ${req.body.name} - Email id: ${req.body.email}\n`;
     
--- routes/contactus.js
+++ routes/contactus.js
@@ -3,13 +3,13 @@ const router=express.Router()
 const path=require('path')
 // const fs=require('fs')
 
-const contactusMain=require('../controller/ccontactus')
+const contactusControllerMain=require('../controller/ccontactus')
 
 //using same routes for get and post requests
-router.get('/contactus',contactusMain.contactusGet)
+router.get('/contactus',contactusControllerMain.contactusControllerGet)
 
 //post route same as contactus
-router.post('/contactus',contactusMain.contactusPost);
+router.post('/contactus',contactusControllerMain.contactusControllerPost);
 
 
 module.exports=router
